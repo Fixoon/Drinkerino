fc46c918d9ffea6ad68e8575dc96678ce2d40dc8
Fixoon <oskar.larsson94@gmail.com>
2018-02-21 00:55:42 +0100

    Changed drinkID to id so it matches Drink.kt

--- app/src/main/java/com/example/oskar/drinkerino/objects/SimpleDrink.kt
+++ app/src/main/java/com/example/oskar/drinkerino/objects/SimpleDrink.kt
@@ -3,5 +3,5 @@ package com.example.oskar.drinkerino.objects
 import com.example.oskar.drinkerino.enums.DrinkGlass
 import com.example.oskar.drinkerino.enums.LikeState
 
-data class SimpleDrink constructor(var name: String, var properties: String, var drinkID:Int,
+data class SimpleDrink constructor(var name: String, var properties: String, var id:Int,
                                    var likeState: LikeState, var drinkGlass: DrinkGlass)
