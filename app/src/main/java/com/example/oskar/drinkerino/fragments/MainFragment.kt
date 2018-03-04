42dc9f06513031b1cde77cd1ce14105210f356d5
Fixoon <oskar.larsson94@gmail.com>
2018-03-04 01:04:53 +0100

    Updated code to match the change from using Dialog to DialogFragment

--- app/src/main/java/com/example/oskar/drinkerino/fragments/MainFragment.kt
+++ app/src/main/java/com/example/oskar/drinkerino/fragments/MainFragment.kt
@@ -1,47 +1,40 @@
 package com.example.oskar.drinkerino.fragments
 
 
-import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
 import android.view.animation.AnimationUtils
-import android.widget.CheckBox
-import android.widget.LinearLayout
-import com.example.oskar.drinkerino.FilterDialog
 import com.example.oskar.drinkerino.R
 import com.example.oskar.drinkerino.activities.RecipeActivity
 import com.example.oskar.drinkerino.adapters.DrinkAdapter
 import com.example.oskar.drinkerino.data.DBHelper
 import com.example.oskar.drinkerino.enums.LikeState
-import com.example.oskar.drinkerino.interfaces.OnLikeClick
-import com.example.oskar.drinkerino.objects.FilterObject
+import com.example.oskar.drinkerino.interfaces.DrinkAdapterLikeAction
+import com.example.oskar.drinkerino.objects.Filter
 import com.example.oskar.drinkerino.objects.SimpleDrink
-import kotlinx.android.synthetic.main.custom_dialog.*
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.item_drink.view.*
 import android.app.Activity
 import android.util.Log
-import android.R.attr.data
+import com.example.oskar.drinkerino.FilterDialogFragment
+import com.example.oskar.drinkerino.interfaces.FilterDialogAction
+import com.example.oskar.drinkerino.objects.FilterDialogCheckboxes
 
 
 
-
-/**
- * Fragment for main tab
- */
-class MainFragment : Fragment(), OnLikeClick {
+class MainFragment : Fragment(), DrinkAdapterLikeAction, FilterDialogAction {
     private lateinit var adapter: DrinkAdapter
-    private lateinit var filterDialog: Dialog
+    private lateinit var filterDialogFrag: FilterDialogFragment
     private val propertiesList = arrayOf("Söt", "Sur", "Besk", "Syrlig", "Salt")
     private val ingredientsList = arrayOf("Rom", "Vodka", "Tequila", "Gin", "Whiskey", "Övriga")
+    private var checkedProperties = arrayListOf(false, false, false, false, false)
+    private var checkedDrinkBases = arrayListOf(false, false, false, false, false, false)
     private var activeDrinkPos = 0
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
@@ -50,17 +43,14 @@ class MainFragment : Fragment(), OnLikeClick {
 
         initializeListView(getDrinksFromDB(LikeState.IGNORE))
 
-        filterDialog = FilterDialog(propertiesList, ingredientsList, activity)
+        filterDialogFrag = FilterDialogFragment()
+        filterDialogFrag.newInstance(propertiesList, ingredientsList, checkedProperties, checkedDrinkBases)
+        filterDialogFrag.setTargetFragment(this, 0)
 
+        Log.d("this is", "a test")
         this.setHasOptionsMenu(true)
     }
 
-    /**
-     * Toggle drinks like state
-     *
-     * @param position Position of drink in current list in adapter
-     * @param retView Drinks main View
-     */
     override fun likeToggle(position:Int, retView:View){
         val drink: SimpleDrink = adapter.getItem(position)
         val db = DBHelper(activity)
@@ -76,7 +66,7 @@ class MainFragment : Fragment(), OnLikeClick {
         }
     }
 
-    private fun getDrinksFromDB(isLiked: LikeState, filter: FilterObject? = null) : ArrayList<SimpleDrink> {
+    private fun getDrinksFromDB(isLiked: LikeState, filter: Filter? = null) : ArrayList<SimpleDrink> {
         val db = DBHelper(activity)
         return db.getDrinksByFilter(isLiked, filter)
     }
@@ -112,52 +102,19 @@ class MainFragment : Fragment(), OnLikeClick {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_filter) {
-            openFilterDialog()
+            filterDialogFrag.show(fragmentManager, "Test")
         }
         return super.onOptionsItemSelected(item)
     }
 
+    override fun filterClick(propertiesDialogCheckboxes: FilterDialogCheckboxes, drinkBaseDialogCheckboxes: FilterDialogCheckboxes, checkOther:Boolean){
+        val filter = Filter(propertiesDialogCheckboxes.checkedBoxesNames, drinkBaseDialogCheckboxes.checkedBoxesNames, checkOther)
+        checkedProperties = propertiesDialogCheckboxes.checkedBoxes
+        checkedDrinkBases = drinkBaseDialogCheckboxes.checkedBoxes
 
-    private fun openFilterDialog(){
-        filterDialog.show()
-
-        filterDialog.cancelButton.setOnClickListener {
-            filterDialog.dismiss()
-        }
-
-        filterDialog.actionButton.setOnClickListener {
-            filterDrinks()
-            filterDialog.dismiss()
-        }
-    }
-
-    private fun filterDrinks(){
-        val propertyViewList:Array<LinearLayout> = arrayOf(filterDialog.propertyLayoutLeft, filterDialog.propertyLayoutRight)
-        val ingredientViewList:Array<LinearLayout> = arrayOf(filterDialog.drinkBaseLayoutLeft, filterDialog.drinkBaseLayoutRight)
-
-        val filter = FilterObject(getCheckedBoxes(propertyViewList),
-                getCheckedBoxes(ingredientViewList),
-                (filterDialog.drinkBaseLayoutRight.getChildAt(2) as CheckBox).isChecked)
+        filterDialogFrag.newInstance(propertiesList, ingredientsList, checkedProperties, checkedDrinkBases)
 
         updateListView(getDrinksFromDB(LikeState.IGNORE, filter))
-    }
-
-    /**
-     * Get text value of all checked checkboxes within views provided
-     *
-     * @param parentViews Parent views of checkboxes
-     */
-    private fun getCheckedBoxes(parentViews:Array<LinearLayout>) : ArrayList<String>{
-        val checkedBoxes:ArrayList<String> = arrayListOf()
-
-        parentViews.forEachIndexed{ index, l ->
-            (0 until l.childCount)
-                    .map { l.getChildAt(it) as CheckBox }
-                    .filter { it.isChecked }
-                    .mapTo(checkedBoxes) { it.text.toString() }
-        }
-
-        return checkedBoxes
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
