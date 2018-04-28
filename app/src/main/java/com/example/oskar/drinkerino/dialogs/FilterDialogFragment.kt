64fa69d7eae6b00a7c66a10c98467ce29e27f8eb
Fixoon <oskar.larsson94@gmail.com>
2018-04-28 17:11:36 +0200

    Switched from using LinearLayout for CheckBoxes to using GridView

--- app/src/main/java/com/example/oskar/drinkerino/dialogs/FilterDialogFragment.kt
+++ app/src/main/java/com/example/oskar/drinkerino/dialogs/FilterDialogFragment.kt
@@ -1,16 +1,14 @@
 package com.example.oskar.drinkerino.dialogs
 
 import android.content.DialogInterface
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.LoaderManager
 import android.support.v4.content.Loader
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.CheckBox
 import com.example.oskar.drinkerino.R
+import com.example.oskar.drinkerino.adapters.CheckBoxAdapter
 import com.example.oskar.drinkerino.interfaces.FilterDialogAction
 import com.example.oskar.drinkerino.interfaces.FilterDialogContract
 import com.example.oskar.drinkerino.presenters.FilterDialogPresenter
@@ -38,14 +36,14 @@ class FilterDialogFragment : DialogFragment(), FilterDialogAction, FilterDialogC
 
         temporaryCheckBoxState = true
 
-        view.cancelButton.setOnClickListener {
+        view!!.cancelButton.setOnClickListener {
             temporaryCheckBoxState = false
             dismiss()
         }
 
         view.actionButton.setOnClickListener {
             val checkBoxes = getCheckedBoxes()
-            mCallback.filterClick(checkBoxes[0] as ArrayList<String>, checkBoxes[1] as ArrayList<String>, (bottomLinearLayoutRight.getChildAt(2) as CheckBox).isChecked)
+            mCallback.filterClick(checkBoxes[0] as ArrayList<String>, checkBoxes[1] as ArrayList<String>, (gridViewBottom.adapter.getItem(5) as CheckBox).isChecked)
             presenter!!.updateCheckedBoxes(checkBoxes[2] as BooleanArray, checkBoxes[3] as BooleanArray)
             temporaryCheckBoxState = false
             dismiss()
@@ -106,76 +104,31 @@ class FilterDialogFragment : DialogFragment(), FilterDialogAction, FilterDialogC
     }
 
     override fun addCheckboxes(topCheckBoxNames: Array<String>, bottomCheckBoxNames: Array<String>, topCheckedBoxes: BooleanArray, bottomCheckedBoxes: BooleanArray) {
-        topLinearLayoutLeft.removeAllViews()
-        topLinearLayoutRight.removeAllViews()
-        bottomLinearLayoutLeft.removeAllViews()
-        bottomLinearLayoutRight.removeAllViews()
-
-        topCheckBoxNames.forEachIndexed { index, s ->
-            val newCheckBox = CheckBox(activity)
-            newCheckBox.isChecked = topCheckedBoxes[index]
-            newCheckBox.text = s
-            if (index % 2 == 1) {
-                view!!.topLinearLayoutRight.addView(newCheckBox)
-            } else {
-                view!!.topLinearLayoutLeft.addView(newCheckBox)
-            }
-        }
-        bottomCheckBoxNames.forEachIndexed { index, s ->
-            val newCheckBox = CheckBox(activity)
-            newCheckBox.isChecked = bottomCheckedBoxes[index]
-            newCheckBox.text = s
-            if (index % 2 == 1) {
-                view!!.bottomLinearLayoutRight.addView(newCheckBox)
-            } else {
-                view!!.bottomLinearLayoutLeft.addView(newCheckBox)
-            }
-        }
+        gridViewTop.adapter = CheckBoxAdapter(context!!, topCheckBoxNames, topCheckedBoxes)
+        gridViewBottom.adapter = CheckBoxAdapter(context!!, bottomCheckBoxNames, bottomCheckedBoxes)
     }
 
     private fun getCheckedBoxes(): Array<Any> {
         val topCheckedBoxesNames: ArrayList<Any> = arrayListOf()
         val bottomCheckedBoxesNames: ArrayList<Any> = arrayListOf()
 
-        val topCheckBoxCount = topLinearLayoutLeft.childCount + topLinearLayoutRight.childCount
-        val bottomCheckBoxCount = bottomLinearLayoutLeft.childCount + bottomLinearLayoutRight.childCount
+        val topCheckBoxStates = BooleanArray(gridViewTop.adapter.count)
+        val bottomCheckBoxStates = BooleanArray(gridViewBottom.adapter.count)
 
-        val topCheckBoxStates = BooleanArray(topCheckBoxCount)
-        val bottomCheckBoxStates = BooleanArray(bottomCheckBoxCount)
 
-        var topIndex = 0
-        for (i in 0 until topCheckBoxCount) {
-            if (i % 2 == 0) {
-                val checkBox = topLinearLayoutLeft.getChildAt(topIndex) as CheckBox
-                topCheckBoxStates[i] = checkBox.isChecked
-                if (checkBox.isChecked) {
-                    topCheckedBoxesNames.add(checkBox.text.toString())
-                }
-            } else {
-                val checkBox = topLinearLayoutRight.getChildAt(topIndex) as CheckBox
-                topCheckBoxStates[i] = checkBox.isChecked
-                if (checkBox.isChecked) {
-                    topCheckedBoxesNames.add(checkBox.text.toString())
-                }
-                topIndex++
+        for (i in 0 until gridViewTop.adapter.count){
+            val checkBox = gridViewTop.adapter.getItem(i) as CheckBox
+            topCheckBoxStates[i] = checkBox.isChecked
+            if(checkBox.isChecked){
+                topCheckedBoxesNames.add(checkBox.text.toString())
             }
         }
 
-        var bottomIndex = 0
-        for (i in 0 until bottomCheckBoxCount) {
-            if (i % 2 == 0) {
-                val checkBox = bottomLinearLayoutLeft.getChildAt(bottomIndex) as CheckBox
-                bottomCheckBoxStates[i] = checkBox.isChecked
-                if (checkBox.isChecked) {
-                    bottomCheckedBoxesNames.add(checkBox.text.toString())
-                }
-            } else {
-                val checkBox = bottomLinearLayoutRight.getChildAt(bottomIndex) as CheckBox
-                bottomCheckBoxStates[i] = checkBox.isChecked
-                if (checkBox.isChecked) {
-                    bottomCheckedBoxesNames.add(checkBox.text.toString())
-                }
-                bottomIndex++
+        for (i in 0 until gridViewBottom.adapter.count){
+            val checkBox = gridViewBottom.adapter.getItem(i) as CheckBox
+            bottomCheckBoxStates[i] = checkBox.isChecked
+            if(checkBox.isChecked){
+                bottomCheckedBoxesNames.add(checkBox.text.toString())
             }
         }
 
